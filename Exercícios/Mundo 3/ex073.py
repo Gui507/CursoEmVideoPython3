c98425ff41b3af8c45b55786fb636b38be9e4686
Gui507 <97295513+Gui507@users.noreply.github.com>
2022-01-10 14:41:21 -0300

    Mundo 3

---
+++ Exercícios/Mundo 3/ex073.py
@@ -0,0 +1,8 @@
+ranking = ('Atlético Mineiro', 'Flamengo', 'Palmeiras', 'Fortaleza', 'Corinthians', 'Bragantino',
+           'Fluminense', 'América - MG', 'Atlético Goianiense', 'Santos', 'Ceará', 'Internacional',
+           'São Paulo', 'Atlético Paranaense', 'Cuiaba', 'Juventude', 'Gremio', 'Bahia', 'Recife', 'Chapecoense')
+print(f'A tabela atua do Campeonato Brasileiro de futebol é: {ranking}')
+print(f'Os cinco primeiros colocados são: {ranking[:5]}')
+print(f'Os quatro ultimos colocados são: {ranking[-4:]}')
+print(f'Os times são: {sorted(ranking)}')
+print(f'A Chapecoense esta na {ranking.index("Chapecoense")+1}ª posição')
