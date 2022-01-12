6c71683ee69633cd787f5f8ebebd4d53825a25d0
Gui507 <97295513+Gui507@users.noreply.github.com>
2022-01-12 11:45:00 -0300

    Mundo 3

--- Exercícios/Mundo 3/ex077.py
+++ Exercícios/Mundo 3/ex077.py
@@ -1,7 +1,6 @@
 palavras = ('aprender', 'ensinar', 'porgramar', 'desenvolver', 'python')
 for p in palavras:
-    print(f'Na palavra {p.capitalize()} temos as vogais: ', end='')
+    print(f'\nNa palavra {p.capitalize()} temos as vogais: ', end='')
     for l in p:
         if l.lower() in 'aeiou':
             print(l,end=' ')
-    print('\n')
