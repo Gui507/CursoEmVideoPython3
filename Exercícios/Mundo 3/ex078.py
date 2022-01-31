ae4ce269845ada6329edc300edf179e4eb0d9da5
Gui507 <97295513+Gui507@users.noreply.github.com>
2022-01-31 18:07:57 -0300

    Mundo 3

--- Exercícios/Mundo 3/ex078.py
+++ Exercícios/Mundo 3/ex078.py
@@ -1,6 +1,20 @@
-n = [int(input('Digite um valor ')),int(input('Digite outro valor ')),
-     int(input('Digite mais um valor ')), int(input('Digite mais um valor ')),
-     int(input('Digite um último valor '))]
-print(f'Você digitou os números\n{n}')
-print(f'o maior valor foi {max(n)}, na posição {n.index(max(n)) + 1}')
-print(f'o maior valor foi {min(n)}, na posição {n.index(min(n)) + 1}')
+numero = []
+maior = menor = 0
+for c in range(0 ,5):
+     numero.append(int(input(f'Digite um valor para a posição {c}: ')))
+     if c == 0:
+          maior = menor = numero[c]
+     else:
+          if numero[c] > maior:
+               maior = numero[c]
+          if numero[c] < menor:
+               menor = numero[c]
+print(f'Você digitou os números: {numero}')
+print(f'O maior valor digitado foi {maior} nas posições: ', end='')
+for i, v in enumerate(numero):
+     if v == maior:
+          print(f'{i}, ', end='')
+print(f'\nO menor valor digitado foi {menor} nas posições: ', end='')
+for i, v in enumerate(numero):
+     if v == menor:
+          print(f'{i}, ',end='')
