1278b9b3c5ac9080485e4fd8814942d96bfd8d7b
Gui507 <97295513+Gui507@users.noreply.github.com>
2022-02-02 18:25:33 -0300

    Mundo 3

---
+++ Exercícios/Mundo 3/ex081.py
@@ -0,0 +1,14 @@
+lista = []
+while True:
+    lista.append(int(input('Digite um número: ')))
+    r = str(input('Deseja continuar? [S/N] ')).strip()
+    if r in 'Nn':
+        break
+    elif r not in 'SsNn':
+        r = str(input('Opção inválida. Deseja continuar? [S/N] ')).strip()
+print('=-'*30)
+print(f'Os valores digitados foram: {lista}.')
+print(f'Foram digitados {len(lista)} números.')
+lista.sort(reverse=True)
+print(f'Em ordem decrescente: {lista}.')
+print('O valor 5 está na lista'if 5 in lista else 'O valor 5 não está na lista')
