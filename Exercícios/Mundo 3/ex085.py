e684afb9362520fecd4cbc58aaf8311b34789d2e
Gui507 <97295513+Gui507@users.noreply.github.com>
2022-02-20 15:18:19 -0300

    Mundo 3

--- Exercícios/Mundo 3/ex085.py
+++ Exercícios/Mundo 3/ex085.py
@@ -1,10 +1,9 @@
-Par = []
-Impar = []
-for c in range(0,7):
-    n = int(input(f'Digite o {c +1}º valor: '))
+núm = [[], []]
+for c in range(1,8):
+    n = int(input(f'Digite o {c}º valor: '))
     if n%2 == 0:
-        Par.append(n)
+        núm[0].append(n)
     else:
-        Impar.append(n)
-print(f'Os valores pare digitados foram: {sorted(Par)}')
-print(f'Os valores Impares digitados foram: {sorted(Impar)}')
+        núm[1].append(n)
+print(f'Os valores pare digitados foram: {sorted(núm[0])}')
+print(f'Os valores Impares digitados foram: {sorted(núm[1])}')
