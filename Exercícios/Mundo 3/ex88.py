373cfe0bcad0a74ea4a12cc1c922894012aaa883
Gui507 <97295513+Gui507@users.noreply.github.com>
2022-02-20 17:00:47 -0300

    Mundo 3

--- Exercícios/Mundo 3/ex88.py
+++ Exercícios/Mundo 3/ex88.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 print('-'*40)
 print('          JOGA NA SENA          ')
 print('-'*40)
@@ -7,6 +8,7 @@ jogos = []
 quantidade = int(input('Quantos jogos voce quer que eu sorteie? '))
 tot = 1
 print('-'*10, f'SORTEANDO {quantidade} JOGOS', '-'*10)
+sleep(1)
 while tot <= quantidade:
     cont = 0
     while True:
@@ -22,3 +24,5 @@ while tot <= quantidade:
     tot += 1
 for i, j in enumerate(jogos):
     print(f'Jogo {i+1}: {j}')
+    sleep(1)
+print('-'*10, 'BOA SORTE', '-'*10)
