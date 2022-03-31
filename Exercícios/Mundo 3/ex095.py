1b05fed59a499aca3b504acd895e603a6b8bdd04
Gui507 <97295513+Gui507@users.noreply.github.com>
2022-03-31 19:58:47 -0300

    Mundo 3

--- Exercícios/Mundo 3/ex095.py
+++ Exercícios/Mundo 3/ex095.py
@@ -3,7 +3,7 @@ gols = list()
 time = []
 while True:
     dados.clear()
-    dados['Nome'] = str(input('Nome: '))
+    dados['Nome'] = str(input('Nome: ')).capitalize()
     dados['Partidas'] = int(input('Quantas partidas ele jogou? '))
     gols.clear()
     for c in range(0, dados['Partidas']):
@@ -20,11 +20,25 @@ while True:
     if r == 'N':
         break
 print('='*80)
-for k , v in enumerate(time):
-    print(f'{k:>4}', end='')
+print('cod ', end='')
+for i in dados.keys():
+    print(f'{i:<15}',end='')
+print()
+print('-'*80)
+for k, v in enumerate(time):
+    print(f'{k:>4} ', end='')
     for d in v.values():
-        print(f'{str(d):>15}', end='')
+        print(f'{str(d):<15}', end='')
     print()
-print('='*80)
-
-
+print('-'*80)
+while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar)'))
+    if busca == 999:
+        break
+    elif busca >= len(time):
+        print('Jogador inexistente')
+    else:
+        print(f"LEVANTAMENTO DO JOGADOR {time[busca]['Nome']}:")
+        for i, g in enumerate(time[busca]['gols']):
+            print(f'{g} gols na partida {i+1}')
+print('Programa encerrado. Obrigado por usar.')
