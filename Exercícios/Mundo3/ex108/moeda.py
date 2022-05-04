8ae779e48853ed5036244ea10242754d891ed557
Gui507 <97295513+Gui507@users.noreply.github.com>
2022-05-04 19:54:58 -0300

    Mundo 3

---
+++ Exercícios/Mundo3/ex108/moeda.py
@@ -0,0 +1,22 @@
+def aumentar(preço=0, taxa=0):
+    res = preço + (taxa/100 * preço)
+    return res
+
+
+def diminuir(preço=0, taxa=0):
+    res = preço - (taxa/100 * preço)
+    return res
+
+
+def dobro(preço=0):
+    res = 2 * preço
+    return res
+
+
+def metade(preço=0):
+    res = preço * 1/2
+    return res
+
+
+def moeda(preço=0, moeda='R$'):
+    return f'{moeda}{preço:.2f}'.replace('.', ',')
