2fa76d55c6a8f10f13765697c25878f48e1ce893
Gui507 <97295513+Gui507@users.noreply.github.com>
2022-05-04 20:25:59 -0300

    Mundo 3

---
+++ Exercícios/Mundo3/ex109/teste.py
@@ -0,0 +1,6 @@
+from Exercícios.Mundo3.ex109 import moeda
+p = float(input('Digite o preço R$'))
+print(f'A metade de R${moeda.moeda(p)} é {moeda.metade(p, True)}')
+print(f'O dobro de R${moeda.moeda(p)} é {moeda.dobro(p, True)}')
+print(f'Aumentando 10% fica {moeda.aumentar(p, 10, True)} ')
+print(f'Dimnuindo 10% fica {moeda.diminuir(p, 10, True)}')
